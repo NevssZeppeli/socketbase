74285ce86b77a7a1224041809470c9df0989111c
Максим Высоцкий <51092227+NevssZeppeli@users.noreply.github.com>
2019-11-26 20:46:36 +0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -3,14 +3,17 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#define PORT 5454
 
+int Connections[100];
+int counter = 0;
 
 int main() 
 {
     int Socket, Connector;
 	sockaddr_in addr;
     addr.sin_family = AF_INET; //тип сети
-    addr.sin_port = htons (7777); //порт
+    addr.sin_port = htons (PORT); //порт
     addr.sin_addr.s_addr = htonl (INADDR_ANY);
 
     Socket = socket (AF_INET, SOCK_STREAM, 0);
@@ -46,7 +49,8 @@ int main()
     {
         std::cout << "Accepted\n";
     }
-
+    char msg[256] = "Hello, blyad";
+    send (Connector, msg, sizeof(msg), 0);
     shutdown (Socket, 2);
     close(Socket);
     
