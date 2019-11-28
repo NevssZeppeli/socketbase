60c05163235ef5be89eaceb8b8329c866b8d0234
Максим Высоцкий <51092227+NevssZeppeli@users.noreply.github.com>
2019-11-28 23:06:28 +0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -3,56 +3,84 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#define PORT 5454
+#include <thread>
+#include <string>
+#define PORT XXXX
 
-int Connections[100];
-int counter = 0;
+int Socket;
+
+void CloseSocket(int sockfd)
+{
+	shutdown(sockfd, 2);
+	close(sockfd);
+}
+
+void ClientHandler (int socketid, int closesocketid)
+{
+	std::cout << "Start speaking: " << std::endl;
+	while (true)
+	{
+	char msg [4096];
+	recv (socketid, msg, sizeof(msg), 0);
+	std::string message = std::string(msg);
+	if (message == "Messaging stopped")
+	{
+		std::cout << "\n--Stop--\n";
+		break;
+		CloseSocket (closesocketid);
+	}
+	std::cout << ">> " << msg << std::endl;
+    }
+}
+
+void sender (int socketid, int closesocketid)
+{
+    while (true)
+    {
+    std::string msg;
+    std::getline (std::cin, msg);
+    std::string exit = "Messaging stopped";
+    if (msg == "/exit")
+    {
+		std::cout << "\n--Stop--\n";
+        send (socketid, exit.c_str(), sizeof(exit), 0);
+		break;
+		CloseSocket (closesocketid);
+
+    }
+    send (socketid, msg.c_str(), sizeof(msg), 0);
+    }
+}
 
 int main() 
 {
-    int Socket, Connector;
 	sockaddr_in addr;
-    addr.sin_family = AF_INET; //тип сети
-    addr.sin_port = htons (PORT); //порт
-    addr.sin_addr.s_addr = htonl (INADDR_ANY);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons (PORT); //порт
+	addr.sin_family = AF_INET; //тип сети
 
-    Socket = socket (AF_INET, SOCK_STREAM, 0);
-    if(Socket < 0)
-    {
-        perror("socket");
-        exit(1);
-    }
-    else 
-    {
-        std::cout << "Socket works\n";
-    }
-    
-    if (bind (Socket, (sockaddr *)&addr, sizeof(addr)) !=0)
-    {
-        perror("bind");
-        exit(2);
-    }
-    else
-    {
-        std::cout << "Bind works\n";
-    }
-    int sizeofaddr = sizeof(addr);
-    listen (Socket, SOMAXCONN);
+	Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(Socket < 0)
+	{
+		perror("socket");
+		exit(1);
+	}
 
-    Connector = accept(Socket, (sockaddr *)&addr, (socklen_t *)&sizeofaddr);
-    if(Socket < 0)
-    {
-        perror("accept");
-        exit(3);
-    }   
-    else
-    {
-        std::cout << "Accepted\n";
-    }
-    char msg[256] = "Hello, blyad";
-    send (Connector, msg, sizeof(msg), 0);
-    shutdown (Socket, 2);
-    close(Socket);
-    
-    return 0;
+	if(connect(Socket, (sockaddr*)&addr, sizeof(addr)) < 0)
+	{
+		perror("connect");
+		exit(2);
+	}
+	else
+	{
+		std::cout << "Connected to server!\n";
+	}
+	std::thread msgrequest (ClientHandler, Socket, Socket);
+	msgrequest.detach();
+	std::thread senderofmsg (sender, Socket, Socket);
+	senderofmsg.join();
+	
+
+
+	return 0;
 }
